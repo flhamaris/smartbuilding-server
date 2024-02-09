fce5bf0c5f3126c9e7f23d5422c53b82d3241420
François-Luc Haghenbeek <francoisluc.haghenbeek@amaris.com>
2024-02-09 10:26:27 +0100

    Update comments.

--- routes/upload.js
+++ routes/upload.js
@@ -14,17 +14,17 @@ var upload = multer({ storage: storage })
 /**
  * POST route handler for uploading a video.
  * The video is expected to be sent as a file in a multipart/form-data request.
- * The request should also include a 'label' field in the body.
+ * The request should also include a 'label' and 'sequenceName' field in the body.
  * The video is sliced into frames, which are saved as PNG images in a directory structure based on the label and a sequence number.
  * If the video is sliced successfully, the function sends a 200 response with a success message.
  * If an error occurs while slicing the video, the function sends a 500 response with an error message.
- * If no file is uploaded or no label is provided, the function sends a 400 response with an error message.
+ * If no file is uploaded or no label or sequenceName is provided, the function sends a 400 response with an error message.
  *
- * @param {object} req - The Express request object. Expected to contain a 'file' field with the uploaded video and a 'label' field in the body.
+ * @param {object} req - The Express request object. Expected to contain a 'file' field with the uploaded video and a 'label' and 'sequenceName' field in the body.
  * @param {object} res - The Express response object. Used to send the response back to the client.
  */
 router.post("/", upload.single("video"), async (req, res) => {
-  // Check if a file and a label were provided. If not, send a 400 response with an error message.
+  // Check if a file, a label, and a sequenceName were provided. If not, send a 400 response with an error message.
   if (!req.file) {
     res.status(400).send({ message: "No file uploaded" })
     return
@@ -38,7 +38,7 @@ router.post("/", upload.single("video"), async (req, res) => {
     return
   }
 
-  // Get the video buffer and label from the request
+  // Get the video buffer, label, and sequenceName from the request
   const videoBuffer = req.file.buffer
   const label = req.body.label
   const sequenceName = req.body.sequenceName
