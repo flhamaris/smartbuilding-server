571a971ac23475ffc13e70e6e445265603e59116
François-Luc Haghenbeek <francoisluc.haghenbeek@amaris.com>
2024-02-13 12:11:39 +0100

    Fix removed route to be still used by app.

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require("morgan")
 var cors = require("cors")
 
 var indexRouter = require("./routes/index")
-var usersRouter = require("./routes/users")
 var uploadRouter = require("./routes/upload")
 
 var app = express()
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use(cors())
 
 app.use("/", indexRouter)
-app.use("/users", usersRouter)
 app.use("/upload", uploadRouter)
 
 // catch 404 and forward to error handler
